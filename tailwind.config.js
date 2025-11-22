b76f9b41f01d0e008f293fc3f36de4d5442564b6
storedistribuidora733-ui <storedistribuidora733@gmail.com>
2025-11-22 06:51:13 -0300

    site cardapio

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+  './styles/**/*.css',
+    './assets/*.js',
+  ],
+  
+  theme: {
+   fontFamily: {
+    'sans': ['Poppins', 'sans-serif']
+   },
+
+    extend: {
+      backgroundImage:{
+        'home': "url('/assets/bg.png')",
+      }
+      },
+    },
+  plugins: [],
+  }
+
