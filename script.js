ceda71390c2987db54f1c9ad93307f8e5d7bd0d0
storedistribuidora733-ui <storedistribuidora733@gmail.com>
2025-11-23 05:14:44 -0300

    Update store hours and fix cart functionality

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const footerBar = document.querySelector("footer");
 const closedAlert = document.getElementById("closed-alert");
 const closedAlertBtn = document.getElementById("closed-alert-btn");
 
-const openingHour = 4;
+const openingHour = 5;
 const closingHour = 22;
 
 const dateSpan = document.getElementById("date-span");
@@ -181,4 +181,5 @@ document.getElementById("checkout-btn").addEventListener("click", () => {
 
     window.open(url, "_blank");
 });
+
 
