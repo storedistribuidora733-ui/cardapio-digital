cdfd23ed300805f978d52b940ba3536d8adc60dd
storedistribuidora733-ui <storedistribuidora733@gmail.com>
2025-12-10 00:04:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ const closedAlertBtn = document.getElementById("closed-alert-btn");
 
 // HORÁRIOS (em minutos desde meia-noite)
 const openingHour = 11 * 60 + 30; // abre 11:30
-const closingHour = 5 * 60;      // fecha 24:00 (meia-noite - você pode ajustar)
+const closingHour = 25 * 60;      // fecha 24:00 (meia-noite - você pode ajustar)
 
 // elemento de status
 const dateSpan = document.getElementById("date-span");
@@ -336,6 +336,7 @@ window.addEventListener("beforeunload", () => {
         // ignore
     }
 });
+
 
 
 
