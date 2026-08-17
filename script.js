4d2597de50bc440f0e9eb80ea6865e1eacbe8788
storedistribuidora733-ui <storedistribuidora733@gmail.com>
2026-08-17 03:41:46 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   horaAbertura: 0,
   horaFechamento: 24,
-  textoStatusAberto: "Aberto até às 24:00",
+  textoStatusAberto: " ",
   textoStatusFechado: "Fechado • Abre às 07:00",
   corStatusAberto: "#22c55e",
   corStatusFechado: "#dc2626",
